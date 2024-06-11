4bfa5229998b45a851f18a72ae851d7e97235749
Oluwadarasimi Adedeji <76637177+SunkenInTime@users.noreply.github.com>
2024-06-11 00:01:58 +0000

    added cell class

---
+++ Cell.java
@@ -0,0 +1,44 @@
+public class Cell {
+
+    private boolean isAlive;
+    private int row;
+    private int col;
+    
+    Cell(int row, int col, boolean isAlive){
+        this.isAlive = isAlive;
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+    public boolean getState(){
+        return isAlive;
+    }
+
+    public void setState(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+    public void setCol(int col) {
+        this.col = col;
+    }
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    @Override
+    public String toString() {
+
+        if (isAlive)
+        return "\033[48;2;0;0;0m";
+
+        return "\033[48;2;255;225;225m";
+
+
+    }
+
+}
--- Grid.java
+++ Grid.java
@@ -1,7 +1,8 @@
+
 public class Grid {
-    boolean[][] grid = new boolean[50][50];
+    Cell[][] grid = new Cell[50][50];
 
-
+    
     int getLiveNeighbors(int row, int col) {
         int liveNeighbors = 0;
         int[][] directions = {
@@ -20,7 +21,7 @@ public class Grid {
             int newCol = col + direction[1];
     
             if (newRow >= 0 && newRow < 50 && newCol >= 0 && newCol < 50) {
-                if (grid[newRow][newCol]) {
+                if (grid[newRow][newCol].getState()) {
                     liveNeighbors++;
                 }
             }
@@ -28,14 +29,17 @@ public class Grid {
         return liveNeighbors;
     }
     
-    boolean getNextState(int row, int col){
+    boolean getNextState(Cell cell){
+        int row = cell.getRow();
+        int col = cell.getCol();
         int liveNeighbors = getLiveNeighbors(row, col);
 
-        if(grid[row][col]){
+        if(grid[row][col].getState){
         if(liveNeighbors >= 2 && liveNeighbors < 4) return true;
            
         
     }else{
+        
         if(liveNeighbors == 3) return true;
     }
         
@@ -44,12 +48,12 @@ public class Grid {
     }
 
     void getNextGeneration(){
-        boolean[][] newGrid = new boolean[50][50];
+        Cell[][] newGrid = new Cell[50][50];
 
         for(int row = 0; row < grid.length; row++){
 
             for(int col = 0; col < grid[row].length; col++){
-                newGrid[row][col] = getNextState(row, col);
+                newGrid[row][col] = newGrid[row][col].setState(getNextState(row, col));
             }
         }
     }
